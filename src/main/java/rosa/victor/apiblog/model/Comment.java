4316045e1346411e2b7d13d3ce35d6013645c82c
victor rosa <vicrosa25@gmail.com>
2022-10-07 09:35:29 +0200

    Create Comment created and tested

--- src/main/java/rosa/victor/apiblog/model/Comment.java
+++ src/main/java/rosa/victor/apiblog/model/Comment.java
@@ -20,7 +20,7 @@ import lombok.NoArgsConstructor;
 public class Comment {
 
   @Id
-  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @GeneratedValue(strategy = GenerationType.AUTO)
   private long id;
 
   private String name;
